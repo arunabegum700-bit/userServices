fb264822a81965432b745f9a29f5b4528a44fbfb
md-abdullah-92 <abdullahalmahadiapurbo@gmail.com>
2026-08-23 23:47:49 +0600

    initial project import

---
+++ src/main/java/org/sust_unknowns/javafest/userservices/config/MailConfiguration.java
@@ -0,0 +1,36 @@
+package org.sust_unknowns.javafest.userservices.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    // Bean for JavaMailSender
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        // Email and password for the mail sender
+        String mail = "abdullahalmahadiapurbo@gmail.com";
+        String password = "kjxupecjjmadhaus";
+
+        // Create and configure JavaMailSenderImpl instance
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com"); // Set SMTP host
+        mailSender.setPort(587); // Set SMTP port
+        mailSender.setUsername(mail); // Set email address
+        mailSender.setPassword(password); // Set email password
+
+        // Set JavaMail properties
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+
+        return mailSender; // Return configured mail sender
+    }
+}
